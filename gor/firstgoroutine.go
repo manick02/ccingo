ea3829f437057f03f4a5cc1e5e6e2597f89659ba
manickavasagam s <manickavasagam.s@myntra.com>
2020-07-02 09:11:52 +0530

    Adding http client example

--- gor/firstgoroutine.go
+++ gor/firstgoroutine.go
@@ -1,5 +1,20 @@
 package main
-
+import ("fmt"
+"sync")
 func main() {
-	$END$
+	var wg sync.WaitGroup
+	x:= func() {
+		defer wg.Done()
+		fmt.Println("I would be used as go routine")
+	}
+    wg.Add(2)
+	go x()
+	go amAnotherGoRoutine(&wg)
+	fmt.Println("hello")
+	wg.Wait()
+}
+
+func amAnotherGoRoutine(wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("I am another go routine")
 }
--- httpclientex/httpclientex.go
+++ httpclientex/httpclientex.go
@@ -1,5 +1,23 @@
 package main
+import ("fmt"
+	"io/ioutil"
+	"log"
+	"net/http")
 
 func main() {
-	$END$
+	fmt.Println("Example of http client")
+	client:=&http.Client{}
+	resp, err :=client.Get("https://example.com")
+
+	if err!=nil {
+		log.Fatal("this is  fatal")
+	} else {
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if (err!=nil) {
+			fmt.Println(err)
+		} else {
+			fmt.Println(string(body))
+		}
+	}
 }
