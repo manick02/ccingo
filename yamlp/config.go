275a416559e7543a2ac5214185ce9ed5dc166847
manickavasagam s <manickavasagam.s@myntra.com>
2020-07-03 17:03:58 +0530

    Fixed yaml parsing issue

--- yamlp/config.go
+++ yamlp/config.go
@@ -3,40 +3,60 @@ package main
 import (
 	"fmt"
 	"gopkg.in/yaml.v2"
-	"os"
+	"io/ioutil"
+	"log"
 )
 
 type Timeout struct {
 	Server int `yaml:"server"`
-	Read int `yaml:"read"`
-	Write int `yaml:"write"`
-	Idle int `yaml:"idle"`
+	Read   int `yaml:"read"`
+	Write  int `yaml:"write"`
+	Idle   int `yaml:"idle"`
 }
 type ServerConfig struct {
 	Timeout Timeout `yaml:"timeout"`
-	Host string `yaml:"host"`
-	Port int `yaml:"port"`
+	Host    string  `yaml:"host"`
+	Port    int     `yaml:"port"`
 }
 
 type Config struct {
-	server ServerConfig `yaml:"server"`
+	Server ServerConfig `yaml:"server"`
+	//Host int `yaml:"host"`
 }
 
-func NewYamlConfig(fileName string) (*Config,error) {
-	config := &Config{}
-	file,err := os.Open(fileName)
-	if err!=nil {
-		return nil,err
-	}
-	defer file.Close()
-	var d = yaml.NewDecoder(file)
-	if err := d.Decode(&config); err != nil {
-		return nil, err
+func NewYamlConfig(fileName string) (*Config, error) {
+	cin := &Config{}
+
+	yamlFile, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		log.Fatalf("yamlFile read err #%v  at path %v", err, fileName)
 	}
 
-	fmt.Println(*config)
+	// Read data from yaml file
+	err = yaml.Unmarshal(yamlFile, cin)
+	if err != nil {
+		log.Fatal("Unable to decode config file", err)
+	}
 
-	return config,nil
+	fmt.Println(cin)
+	return cin, nil
+	//
+	//
+	//config := &Config{}
+	//file,err := ioutil.ReadFile(fileName)
+	//if err!=nil {
+	//	return nil,err
+	//}
+	//err = yaml.Unmarshal(file, config)
+	//fmt.Println(config)
+	//fmt.Println("----")
+	//if err != nil {
+	//	fmt.Println("Error happened while parsing file",err)
+	//	return nil, err
+	//}
+	//
+	//fmt.Println(*config)
+	//return config,nil
 }
 
 //host: 127.0.0.1
--- yamlp/parseyaml.go
+++ yamlp/parseyaml.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	filename:="/Users/8420/workspace/goprojects/src/github.com/manick02/ccingo/yamlp/config.yaml"
+	filename := "/Users/8420/workspace/goprojects/src/github.com/manick02/ccingo/yamlp/config.yaml"
 	fmt.Println(filename)
-	config,err:=NewYamlConfig(filename)
-    if (err!=nil) {
-	 fmt.Println(config)
-    }
-    fmt.Println(err)
+	config, err := NewYamlConfig(filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(*config)
 }
