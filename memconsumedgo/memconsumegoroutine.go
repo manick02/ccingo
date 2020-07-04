05249e30c69a42581ec88c0eca4bac79af6316c0
manickavasagam s <manickavasagam.s@myntra.com>
2020-07-04 19:57:10 +0530

    Memory consumed by go routine

---
+++ memconsumedgo/memconsumegoroutine.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	var c <-chan interface{}
+	var wg sync.WaitGroup
+	noop := func() {
+		wg.Done()
+		<-c
+	}
+	const numRoutines = 1e6
+	wg.Add(numRoutines)
+	before := memconsumed()
+	for i := 0; i < numRoutines; i++ {
+		go noop()
+	}
+	wg.Wait()
+	after := memconsumed()
+	fmt.Println()
+	fmt.Printf("memory used per goroutine: %.3f kb\n", float64(after-before)/(numRoutines*1024))
+}
+
+func memconsumed() uint64 {
+	runtime.GC()
+	var s runtime.MemStats
+	runtime.ReadMemStats(&s)
+	return s.Sys
+}
