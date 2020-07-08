3cf525f1c06c67cc15c10a4cb9e99e3f0d01d444
manickavasagam s <manickavasagam.s@myntra.com>
2020-07-08 10:40:08 +0530

    Added simple go routine

---
+++ anothersimplegoroutine/anothersimplegoroutine.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	for i:= range []int {1,3,4,5} {
+		wg.Add(1)
+		go func(i int) {
+            defer wg.Done()
+			fmt.Println(i)
+		}(i)
+	}
+
+	fmt.Println("Waiting for go routines to finish")
+	wg.Wait()
+	fmt.Println("Go routines are complete")
+}
